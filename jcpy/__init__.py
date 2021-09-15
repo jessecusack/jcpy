ddead269faee894e0ed0e799d4b1448584039382
Jesse Cusack <jmcusack@marine.rutgers.edu>
2021-09-15 13:01:05 -0400

    minor style adjustments

--- jcpy/__init__.py
+++ jcpy/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for jcpy."""
 
 __author__ = """Jesse Cusack"""
-__email__ = 'jmcusack@marine.rutgers.edu'
-__version__ = '0.1.0'
+__email__ = "jmcusack@marine.rutgers.edu"
+__version__ = "0.1.0"
--- tests/test_jcpy.py
+++ tests/test_jcpy.py
@@ -4,7 +4,6 @@
 
 import pytest
 
-
 from jcpy import jcpy
 
 
